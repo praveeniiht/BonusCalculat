2a66bcb9d281c0f9ece9c2e096f14a603198ba59
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2020-10-10 16:48:48 +0530

    Add files via upload

---
+++ src/main/java/yaksha/BonusCalculator.java
@@ -0,0 +1,15 @@
+package yaksha;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeMap;
+
+public class BonusCalculator {
+	public static TreeMap<Integer, Integer> calculateBonus(HashMap<Integer, String> hm, HashMap<Integer, Integer> hm1) {
+		return null;
+	}
+
+}
---
+++ src/main/java/yaksha/MainClass.java
@@ -0,0 +1,12 @@
+package yaksha;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class MainClass {
+	public static void main(String args[]) {
+		
+	}
+}
